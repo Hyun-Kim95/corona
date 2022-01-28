9fd48ec2059c0bc239d3e0667ce5bd7a8bb94c92
Hyun-Kim95 <75933619+Hyun-Kim95@users.noreply.github.com>
2022-01-28 14:16:54 +0900

    세팅

---
+++ src/main/java/com/toy/kh/coronaCheck/CoronaCheckApplication.java
@@ -0,0 +1,13 @@
+package com.toy.kh.coronaCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaCheckApplication.class, args);
+	}
+
+}
